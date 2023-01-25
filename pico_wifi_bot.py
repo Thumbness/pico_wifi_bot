f07aff51cd41772d34221ada80fbe5ef6a2db7bf
Thumbness <face.first@hotmail.com>
2023-01-25 23:57:38 +1100

    Add pico_server capability and HTML for interal LED switching

--- pico_wifi_bot.py
+++ pico_wifi_bot.py
@@ -7,8 +7,6 @@ import machine
 ssid = 'Sherwizzle'
 WPA2 = 'tofuopie8'
 
-
-
 def connect():
     #Connect to WLAN
     wlan = network.WLAN(network.STA_IF)
@@ -18,15 +16,74 @@ def connect():
     for line in networks:
         print(line[0].decode())
         wlan.connect(ssid, WPA2)
+        
     while wlan.isconnected() == False:
         print('Waiting for connection...')
         sleep(1)
+        
     pico_ip = wlan.ifconfig()[0]
     print(f'connected on {pico_ip}')
     return pico_ip
+
+def open_socket(pico_ip):
+    address = (pico_ip, 80)
+    connection = socket.socket()
+    connection.bind(address)
+    connection.listen(1)
+    print(connection)
+    return connection
+
+def webpage(temperature, state):
+    #Template HTML
+    html = f"""
+            <!DOCTYPE html>
+            <html>
+
+            <body>
+                <form action="./lighton">
+                    <input type="submit" value="Light on" />
+                </form>
+                <form action="./lightoff">
+                    <input type="submit" value="Light off" />
+                </form>
+                <p>LED is {state} </p>
+                <p>Temperature is {temperature}</p>
+            </body>
+
+            </html>
+            """
+    return str(html)
+
+def serve(connection):
+    #Start a web server
+    state = 'OFF'
+    pico_led.off()
+    temperature = 0
+    while True:
+        client = connection.accept()[0]
+        request = client.recv(1024)
+        request = str(request)
+        try:
+            request = request.split()[1]
+        except IndexError:
+            pass
+        if request == '/lighton?':
+            pico_led.on()
+            state = 'ON'
+        elif request == '/lightoff?':
+            pico_led.off()
+            state = 'OFF'
+        temperature = pico_temp_sensor.temp
+        print(request)
+        html = webpage(temperature, state)
+        client.send(html)
+        client.close()
         
 try:
-    connect()
+    pico_ip = connect()
+    connection = open_socket(pico_ip)
+    serve(connection)
+    
 except KeyboardInterrupt:
     machine.reset()
     
